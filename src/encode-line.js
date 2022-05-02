3adc37fb0edefc06c2d40b3947c9c1c040c2198f
Artyom Ilyin <ilyin@infostorm.ru>
2022-05-03 01:27:02 +0300

    feat: encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+    let result = [];
+    let count = 1;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == str[i + 1]) {
+            count++;
+        } else if (count > 1) {
+            result.push(count);
+            result.push(str[i]);
+            count = 1;
+        } else {
+            result.push(str[i]);
+        }
+    }
+    return result.join('');
 }
 
 module.exports = {
-  encodeLine
+    encodeLine,
 };
