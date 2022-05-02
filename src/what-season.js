9e0e39f8fcc8adce503f506392edc4ac710db44b
Artyom Ilyin <ilyin@infostorm.ru>
2022-05-02 18:13:14 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -2,20 +2,41 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+    if (!date) return 'Unable to determine the time of year!';
+    if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0)
+        throw new Error('Invalid date!');
+    switch (date.getMonth() + 1) {
+        case 12:
+        case 1:
+        case 2:
+            return 'winter';
+        case 3:
+        case 4:
+        case 5:
+            return 'spring';
+        case 6:
+        case 7:
+        case 8:
+            return 'summer';
+        case 9:
+        case 10:
+        case 11:
+            return 'autumn';
+        default:
+            break;
+    }
 }
 
 module.exports = {
-  getSeason
+    getSeason,
 };
